6153e64332129320625480d0147ab072fa8f116a
lucio rendine <luciorendine69@gmail.com>
2021-03-03 17:18:33 +0100

    push

--- database/database.php
+++ database/database.php
@@ -4,37 +4,43 @@ $album = [
    'cover' => "img/metallifting.jpg",
    'title' => "Motorhead",
    'artist' => "Motorhead",
-   'year' => "1988"
+   'year' => "1988",
+   'genre' => 'metal'
   ],
   [
    'cover' => "img/metallifting.jpg",
    'title' => "Motorizer",
    'artist' => "Motorhead",
-   'year' => "2010"
+   'year' => "2010",
+   'genre' => 'metal'
   ],
   [
    'cover' => "img/metallifting.jpg",
    'title' => "Bad magic",
    'artist' => "Motorhead",
-   'year' => "2015"
+   'year' => "2015",
+   'genre' => 'rock'
   ],
   [
    'cover' => "img/metallifting.jpg",
    'title' => "Bomber",
    'artist' => "Motorhead",
-   'year' => "1979"
+   'year' => "1979",
+   'genre' => 'rock'
   ],
   [
    'cover' => "img/metallifting.jpg",
    'title' => "Overkill",
    'artist' => "Motorhead",
-   'year' => "1979"
+   'year' => "1979",
+   'genre' => 'hardrock'
   ],
   [
    'cover' => "img/metallifting.jpg",
    'title' => "Inferno",
    'artist' => "Motorhead",
-   'year' => "2004"
+   'year' => "2004",
+   'genre' => 'metal'
   ],
-] 
+]
  ?>
--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 new Vue({
   el:'#root',
   data: {
-    listAlbum:[]
+    listAlbum:[],
+    userGenre:''
   },
-  mounted() {
-    const self = this;
-    axios.get("http://localhost/php-ajax-dischi/app/server.php")
-    .then(function(response) {
-      self.listAlbum = response.data;
-    });
+  // mounted() {
+  //   const self = this;
+  //   axios.get("http://localhost/php-ajax-dischi/app/server.php")
+  //   .then(function(response) {
+  //     self.listAlbum = response.data;
+  //   });
+  // },
+  method:{
+    search:function(){
+      const self = this;
+      axios.get("http://localhost/php-ajax-dischi/app/server.php?genre" + self.userGenre)
+      .then(function(response) {
+        self.listAlbum = response.data;
+      });
+    }
   },
 })
 Vue.config.devtools = true
