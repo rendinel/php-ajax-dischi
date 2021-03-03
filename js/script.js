97c0f393ab97bf42389cef361c86583326ab58f9
lucio rendine <luciorendine69@gmail.com>
2021-03-03 11:49:32 +0100

    push

---
+++ js/script.js
@@ -0,0 +1,17 @@
+new Vue({
+  el:'#root',
+  data: {
+    listAlbum:[],
+  },
+  mounted() {
+    const self = this;
+    axios.get("localhost/php-ajax-dischi/app/server.php")
+    .then(function(response) {
+      self.listAlbum = response.data;
+    });
+  },
+  methods:{
+
+  }
+});
+Vue.config.devtools = true
